4da03d36c9dafb07816d2233fcf298065c95ef7c
Lucas Segers <lucas.segers04@gmail.com>
2024-07-14 19:17:38 -0300

    add: todo test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -48,7 +48,7 @@ describe('AppController', () => {
       .expect(200);
   });
 
-  it('also accepts external provider jwts', async () => {
+  it('also accepts external provider jwts (fixed token)', async () => {
     const jwt =
       'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6InBiX1RLUlhWSlktMjd2SUdfQTgxUE1ILWNkWSJ9.eyJ2ZXIiOiIyLjAiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vOTE4ODA0MGQtNmM2Ny00YzViLWIxMTItMzZhMzA0YjY2ZGFkL3YyLjAiLCJzdWIiOiJBQUFBQUFBQUFBQUFBQUFBQUFBQUFNbmx2aGdPcERwNlJ6MVdEbmRRc0VBIiwiYXVkIjoiOThmMTk5OTMtNzUzZS00ZWFlLWE3NTgtOTk1MzdhMDU0YmU5IiwiZXhwIjoxNzIxMDgwNTcyLCJpYXQiOjE3MjA5OTM4NzIsIm5iZiI6MTcyMDk5Mzg3MiwiZW1haWwiOiJsdWNhcy5zZWdlcnMwNEBnbWFpbC5jb20iLCJ0aWQiOiI5MTg4MDQwZC02YzY3LTRjNWItYjExMi0zNmEzMDRiNjZkYWQiLCJub25jZSI6IjYzYjBkZTFhLWJkZjItNGRlYy1iOTU2LThiMWNhZmUzYTY3MyIsImFpbyI6IkR2UiF3cjVYU1YqMmV6aFhGcmJ1bjN5KnFUc0VSNThQckZtZ3p4UDJLc0phS3Z3SEVsZDd6U21NZE52Y2piSFJtYmdIM1Jrbng3d05sbG16U00xcjNxa2lPOWZBSWJQITQyNTJzVWgzVTAzcVQ4eW0yVkl0aldyeGJUeHQ2cnozT21iWUNrMmg4b2xhSUFxWjAqcEJsS2oyYUNLYU0hWDZVYVljSnA0anN4bjFTSEt3M2E3aU5sSk1RSzc5S3QqWTJBJCQifQ.M2EGAcIAvhKaZJyObHsJcJewqmwgiB2FI-FmFOdU-XF7NXvcT_b950fxW1pBVcVy6VS3PVtLd21U-Etk1-CABl3HI-0N75E235SVLj6yQQA5PwvsBSPnJYLNJJzwgt5gKr1nUQZaylsCebNIft_Dta7qr_0iEtNBY9fCaEf53gGRRaENh0gZ5dTNU8wDWEn8ALX6gI4Fphp-gwBZcHXSHzoa169ugaxIWdFc62KMwKbN9S_7FhwznFMqTgzxX8AbQ6BrsPavndsPowjgg5oUCmS1tvLCztT_DJTiMe9KXy9jBP3OuY8eOHF7Z4g5m1pZwwjGEKNZIqurX-SgGZ-gHQ';
 
@@ -57,4 +57,6 @@ describe('AppController', () => {
       .set({ Authorization: `Bearer ${jwt}` })
       .expect(200);
   });
+
+  it.todo('accepts external provider jwts');
 });
